10cc1db228dab70361aad011833286560520dd8a
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-10-01 14:42:37 -0500

    Minor progress

--- src/jsonpp/jsonpp.hpp
+++ src/jsonpp/jsonpp.hpp
@@ -1,9 +1,20 @@
 #ifndef JavaScriptObjectNotationPlusPlus_HeaderPlusPlus
 #define JavaScriptObjectNotationPlusPlus_HeaderPlusPlus
+#include <memory>
 
 namespace jsonpp
 {
-	//
+	namespace _ //private
+	{
+		template<typename D>
+		struct PImpl { struct Impl; };
+	}
+	struct Environment
+	: private                 _::PImpl<Environment>
+	, private std::unique_ptr<_::PImpl<Environment>::Impl>
+	{
+		//
+	};
 }
 
 #endif
